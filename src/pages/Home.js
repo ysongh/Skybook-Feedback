683234b1768b71c7ea2006a4dc5d025cd157dac6
You Song Hou <yousonghou@yahoo.com>
2021-04-17 23:12:42 -0400

    add MySky login and logout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,71 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SkynetClient } from "skynet-js";
 
-const client = new SkynetClient("https://siasky.net");
+const portal = 'https://siasky.net/';   // allow for developing on localhost
+const client = new SkynetClient(portal);
 const hostApp = "host-app.hns";
+const dataDomain = 'localhost';
 
 function Home() {
+  const [userID, setUserID] = useState();
+  const [mySky, setMySky] = useState();
+  const [loggedIn, setLoggedIn] = useState(null);
+
   useEffect(() => {
-    async function mySkyExample() {
+    async function initMySky() {
       try {
-        // Initialize MySky.
-        const mySky = await client.loadMySky(hostApp);
-        console.log(mySky)
-      } catch (error) {
-        console.log(error)
+        // load invisible iframe and define app's data domain
+        // needed for permissions write
+        const mySky = await client.loadMySky(dataDomain);
+    
+        // check if user is already logged in with permissions
+        const loggedIn = await mySky.checkLogin();
+
+        setMySky(mySky);
+        setLoggedIn(loggedIn);
+        if (loggedIn) {
+          setUserID(await mySky.userID());
+        }
+      } catch (e) {
+        console.error(e);
       }
     }
-    mySkyExample();
+    initMySky();
   }, [])
-  
-  async function checkLoginExample() {
-    try {
-      const mySky = await client.loadMySky(hostApp);
-  
-      // Try to login silently, requesting permissions for host-app.hns.
-      const loggedIn = await mySky.checkLogin();
-      console.log(loggedIn)
-    } catch (error) {
-      console.log(error)
+
+  const handleMySkyLogin = async () => {
+    // Try login again, opening pop-up. Returns true if successful
+    const status = await mySky.requestLoginAccess();
+
+    setLoggedIn(status);
+
+    if (status) {
+      setUserID(await mySky.userID());
     }
-  }
+  };
+  
+  const handleMySkyLogout = async () => {
+    // call logout to globally logout of mysky
+    await mySky.logout();
+
+    setLoggedIn(false);
+    setUserID('');
+  };
 
   return (
     <div>
-      <button onClick={checkLoginExample}>Check Login</button>
+      <p>userID: {userID}</p>
+      {/* <p>mySky: {mySky}</p> */}
+      {loggedIn === true && (
+        <button onClick={handleMySkyLogout}>
+          Log Out of MySky
+        </button>
+      )}
+      {loggedIn === false && (
+        <button onClick={handleMySkyLogin}>
+          Login with MySky
+        </button>
+      )}
     </div>
   );
 }
