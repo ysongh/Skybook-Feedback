7c04c99e28883d1e7a581b666f9340161c359960
You Song Hou <yousonghou@yahoo.com>
2021-04-18 01:04:44 -0400

    add functions to get and set user data in user's filesystem on MySky

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,8 @@ function Home() {
   const [userID, setUserID] = useState();
   const [mySky, setMySky] = useState();
   const [loggedIn, setLoggedIn] = useState(null);
+  const [message, setMessage] = useState("");
+  const [text, setText] = useState("");
 
   useEffect(() => {
     async function initMySky() {
@@ -52,20 +54,52 @@ function Home() {
     setUserID('');
   };
 
+  const getJSONExample = async () => {
+    try {
+      // Get discoverable JSON data from the given path.
+      const { data, skylink } = await mySky.getJSON("app.hns/path/file.json");
+      console.log(data, skylink);
+      setMessage(data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const setJSONExample = async () => {
+    try {
+      // Set discoverable JSON data at the given path. The return type is the same as getJSON.
+      const { data, skylink } = await mySky.setJSON("app.hns/path/file.json", { message: text });
+      console.log(data, skylink);
+      setMessage(data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div>
       <p>userID: {userID}</p>
       {/* <p>mySky: {mySky}</p> */}
       {loggedIn === true && (
-        <button onClick={handleMySkyLogout}>
-          Log Out of MySky
-        </button>
+        <>
+          <button onClick={handleMySkyLogout}>
+            Log Out of MySky
+          </button>
+          <button onClick={getJSONExample}>
+            Get Data
+          </button>
+          <button onClick={setJSONExample}>
+            Set Data
+          </button>
+          <input onChange={(e) => setText(e.target.value)} value={text} placeholder="Enter text" />
+        </>
       )}
       {loggedIn === false && (
         <button onClick={handleMySkyLogin}>
           Login with MySky
         </button>
       )}
+      <p>{message}</p>
     </div>
   );
 }
