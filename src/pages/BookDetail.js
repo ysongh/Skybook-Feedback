5ef181ecb6868607d08192bb78636730340edace
You Song Hou <yousonghou@yahoo.com>
2021-05-11 00:28:39 -0400

    fix a bug on the book detail page when refreshing

--- src/pages/BookDetail.js
+++ src/pages/BookDetail.js
@@ -13,11 +13,12 @@ function BookDetail() {
   const { id } = useParams();
   const { state = {} } = useLocation();
 
+  const [book, setBooks] = useState({});
   const [comments, setComments] = useState([]);
   const [comment, setComment] = useState("");
   const [loading, setLoading] = useState(false);
   const [commentLoading, setCommentLoading] = useState(false);
-  const [likes, setLikes] = useState(state.selectedBook.likes.length);
+  const [likes, setLikes] = useState([]);
 
   useEffect(() => {
     async function getCommentsFromSkyDB() {
@@ -42,6 +43,23 @@ function BookDetail() {
     }
 
     getCommentsFromSkyDB();
+  }, [])
+
+  useEffect(() => {
+    async function getBookFromSkyDB() {
+      const { data, skylink } = await clientSkyDB.db.getJSON(publicKey, "books");
+      console.log(data, skylink);
+      setBooks(data.books[id]);
+      setLikes(data.books[id].likes.length);
+    }
+
+    if(state.selectedBook) {
+      setBooks(state.selectedBook);
+      setLikes(state.selectedBook.likes.length);
+    }
+    else{
+      getBookFromSkyDB();
+    }
   }, [])
 
 
@@ -126,39 +144,41 @@ function BookDetail() {
       <br />
 
       <Grid>
-        <Grid.Column mobile={16} tablet={16} computer={10}>
-          <Card.Group>
-            <Card fluid>
-              <Card.Content>
-                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '1rem' }}>
-                  <Card.Header style={{ fontSize: '1.75rem' }}>{state.selectedBook.title}</Card.Header>
-                  <Card.Meta>{state.selectedBook.date}</Card.Meta>
-                </div>
-                <Card.Description>
-                  {state.selectedBook.preview}
-                </Card.Description>
-                <br />
-                <ReactQuill className="hideToolbar" theme="snow" value={state.selectedBook.body} readOnly/>
-                <br />
-                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                  <Label as='a' image>
-                    <img src='/images/defaultuser.png' />
-                    {state.selectedBook.author}
-                  </Label>
-                  <Button as='div' labelPosition='right' onClick={likeABookOnSkyDB}>
-                    <Button color='red'>
-                      <Icon name='heart' />
-                      Like
-                    </Button>
-                    <Label as='a' basic color='red' pointing='left'>
-                      {likes}
+        {book.title && (
+          <Grid.Column mobile={16} tablet={16} computer={10}>
+            <Card.Group>
+              <Card fluid>
+                <Card.Content>
+                  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '1rem' }}>
+                    <Card.Header style={{ fontSize: '1.75rem' }}>{book.title}</Card.Header>
+                    <Card.Meta>{book.date}</Card.Meta>
+                  </div>
+                  <Card.Description>
+                    {book.preview}
+                  </Card.Description>
+                  <br />
+                  <ReactQuill className="hideToolbar" theme="snow" value={book.body} readOnly/>
+                  <br />
+                  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                    <Label as='a' image>
+                      <img src='/images/defaultuser.png' />
+                      {book.author}
                     </Label>
-                  </Button>
-                </div>
-              </Card.Content>
-            </Card>
-          </Card.Group>
-        </Grid.Column>
+                    <Button as='div' labelPosition='right' onClick={likeABookOnSkyDB}>
+                      <Button color='red'>
+                        <Icon name='heart' />
+                        Like
+                      </Button>
+                      <Label as='a' basic color='red' pointing='left'>
+                        {likes}
+                      </Label>
+                    </Button>
+                  </div>
+                </Card.Content>
+              </Card>
+            </Card.Group>
+          </Grid.Column>
+        )}
         <Grid.Column mobile={16} tablet={16} computer={6}>
           <Header as='h3' dividing>
             Comments
